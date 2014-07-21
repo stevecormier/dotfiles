20c3e890dac8b4bb1a7c0271bac4af507083ec7b
Stephen Cormier <sjocormier@gmail.com>
2014-07-20 22:47:17 -0400

    ssd tweaks

--- osx/osx-defaults.sh
+++ osx/osx-defaults.sh
@@ -2,19 +2,24 @@
 
 # Always use Finder's list view
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+# Show volumes on desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediasOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bol true
+# enable tap to click
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # SSD-specific tweaks
 
 # Disable local Time Machine snapshots
 sudo tmutil disablelocal
-
-# Disable hibernation (speed up enterins sleep mode)
+# Disable hibernation (speed up entering sleep mode)
 sudo pmset -a hibernatemode 0
-
+# Turn off sudden motion sensor
+sudo pmset -a sms 0
+# Remove sleep image file
 sudo rm /Private/var/vm/sleepimage
+# and replace with blank file
 sudo touch /Private/var/vm/sleepimage
 sudo chflags uchg /Private/var/vm/sleepimage
-
-sudo pmset -a sms 0
-
 
