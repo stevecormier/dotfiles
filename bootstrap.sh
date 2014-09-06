5aabdb66836c42cf0617b39fadf2a6d7d3472523
Stephen Cormier <sjocormier@gmail.com>
2014-09-06 16:20:25 -0400

    add vim dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 dir=~/dotfiles
-files="vimrc"
+files="vimrc vim"
 
 echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 for file in $files; do
 	echo "Creating symlink to $file in home directory."
