d6ad31a27ef6f654788e7d158baa14fd3dc0dffe
Stephen Cormier <stephen.cormier@makerbot.com>
2014-11-17 15:04:49 -0500

    rename some stuff

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 dir=~/dotfiles
-files="vimrc vim bashrc bash_profile"
+files="vimrc vim zshrc"
 
 for file in $files; do
-	echo "Creating symlink to $file in home directory."
-	ln -s $dir/$file ~/.$file
+	echo "Creating symlink to .$file in home directory."
+	ln -s $dir/.$file ~/.$file
 done
